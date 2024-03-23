9a7e07f1092f398f98883feb9e8899abaecce6e0
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2024-03-23 21:30:00 +0900

    study :: first commit and user story.md update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+// 유저스토리
+// 1. 유저는 현재 위치의 날씨를 볼 수 있다.
+// 1-1. 날씨 정보에는 도시이름, 섭씨, 화씨, 날씨상태 정보가 보여짐
+// 2. 5개의 버튼이 있는데, 하나는 현재위치 그리고 4개는 내가 원하는 도시로 총 5개로 구성
+// 3. 유저는 다른 도시 버튼을 클릭하면 해당 도시의 날씨 정보를 볼 수 있다.
+// 4. 유저는 데이터가 로딩될 때 로딩 스피너를 볼 수 있다.
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const weahterAPI = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={02e8eecc84dea504dee388ebca659896}`;
+    return <div>hi</div>;
 }
 
 export default App;
