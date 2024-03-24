6067ca688fa7379ba96ddb79125a69a318d1261f
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2024-03-24 23:25:18 +0900

    update :: Celsius to Fahrenheit

--- src/component/WeatherText.js
+++ src/component/WeatherText.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const WeatherText = ({ weatherData }) => {
+    const temp = parseInt(weatherData?.main.temp);
+    const fahrenheit = Math.floor((temp * 9) / 5 + 32);
+
     return (
         <div className="currentWeather">
             <p className="cityName">{weatherData?.name}</p>
-            <h1 className="temp">{weatherData?.main.temp}C</h1>
+            <h1 className="temp">
+                {temp}
+                <span className="fontSpan">&#x2103;</span> / {fahrenheit}
+                <span className="fontSpan">&#8457;</span>
+            </h1>
             <p className="weatherState">
-                {weatherData?.name} is {weatherData?.weather[0].description}
+                <span className="cityName">{weatherData?.name}</span> is {weatherData?.weather[0].description}
             </p>
         </div>
     );
